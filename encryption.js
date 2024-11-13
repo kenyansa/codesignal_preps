38ee40e89545ff943ba9a9277455f221edcdb0cd
kenyansa <kenyansajustine@gmail.com>
2024-11-13 13:24:42 +0300

    feat: :fire: create an encryption program using string manipulation methods

---
+++ encryption.js
@@ -0,0 +1,30 @@
+function encryptString(text) {
+    let encrypted = '';
+    for (let i = 0; i < text.length; i++) {
+        let c = text.charAt(i);
+        
+        // Check if `c` is a lowercase letter and not 'z'
+        if (/^[a-y]$/.test(c)) {
+            encrypted += String.fromCharCode(c.charCodeAt(0) + 1);
+        }
+        // Check if `c` is 'z', and change it to 'a'
+        else if (c === 'z') {
+            encrypted += 'a';
+        }
+        // Check if `c` is an uppercase letter and not 'Z'
+        else if (/^[A-Y]$/.test(c)) {
+            encrypted += String.fromCharCode(c.charCodeAt(0) + 1);
+        }
+        // Check if `c` is 'Z', and change it to 'A'
+        else if (c === 'Z') {
+            encrypted += 'A';
+        }
+        // If it's not a letter, append it unchanged
+        else {
+            encrypted += c;
+        }
+    }
+    return encrypted;
+}
+
+console.log("The encrypted text is: " + encryptString("Hello, Java!")); // Should print out "Ifmmp, Kbwb!"
