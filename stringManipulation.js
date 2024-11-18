2c4f250dee3d13779c2e82cce43b86372f148c09
kenyansa <kenyansajustine@gmail.com>
2024-11-18 15:36:25 +0300

    feat: :sparkles: space exploration string manipulation

--- stringManipulation.js
+++ stringManipulation.js
@@ -28,3 +28,20 @@ employeeList.forEach(employee => {
     
     let astronautData = "    Neil-Mars; Buzz-Jupiter; Sally-Venus    ";
     processAstronautData(astronautData);
+
+    // In our final task, you will process space exploration crew members' data for command assignments! You have been given a string of crew details separated by semicolons. Your mission is to split, clean, and display each member's information correctly. Let's get those crew details ready for launch!
+    // Space exploration crew members' data, containing their names, missions, and roles
+let crewData = "Neil,Armstrong,Apollo 11,C;Buzz,Aldrin,Apollo 11,P;Michael,Collins,Apollo 11,CM";
+const crew = crewData.split(";")
+// TODO: Split the crewData string into a list of individual crew member information using the appropriate delimiter
+// TODO: Iterate over the list of crew member data
+// TODO: For each member, split their data string using commas as delimiters
+// TODO: Print the crew member's details in a formatted string
+crew.forEach(member=>{
+    const[firstname, lastname, mission, role] = member.split(",")
+    console.log(`${firstname} ${lastname} ${mission} ${role}`)
+});
+// Expected output:
+// Neil Armstrong Apollo 11 C
+// Buzz Aldrin Apollo 11 P
+// Michael Collins Apollo 11 CM
