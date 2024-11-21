9a1bbea6d693d0239652df4f2c0b6d21be62aea3
kenyansa <kenyansajustine@gmail.com>
2024-11-21 12:40:22 +0300

    feat: :sparkles: new solution to reverse countnerparts

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -19,43 +19,58 @@ console.log(solution(numbers));
 // You are provided with a list of n integers, where n ranges from 2 to 200, inclusive. The task is to return a list of pairs, each containing an integer and its reverse counterpart. In this context, the reverse counterpart of a number is the number with its digits reversed. For example, the reverse counterpart of 123 is 321.You must iterate through the list to find the reverse counterpart for each integer. If this reversed number exists in the list, create a pair with the original number and its reverse counterpart. If not, skip it. Your output should be a list of pairs with the original integers and their reverse counterparts. The integers in the given list will range from 10 to 9999, inclusive, and each integer in the list is unique. Note: The reverse counterpart of a single digit number is the same number. For numbers that result in leading zeros when reversed (e.g., 100 becomes 001, which is 1), consider only the integer value (i.e., 1). It's guaranteed that the original list will not contain integers with leading zeros. For example, for numbers = {12, 21, 34, 43, 56, 65}, the output should be solution(numbers) = {{12, 21}, {21, 12}, {34, 43}, {43, 34}, {56, 65}, {65, 56}}.
 
 function solution(nums) {
-    let pairs = []; // Store pairs
-    let seen = new Set(); // Track processed numbers
+    const result = [];
+    const numSet = new Set(nums);
 
-    // Count occurrences of each number
-    let countMap = nums.reduce((map, num) => {
-        map[num] = (map[num] || 0) + 1;
-        return map;
-    }, {});
-
-    for (let num of nums) {
-        if (seen.has(num)) 
-            continue; // Skip already processed numbers
-
-        let reversed = parseInt(num.toString().split("").reverse().join(""));
-
-        if (num === reversed) {
-            // Palindrome: add as many times as it appears
-            for (let i = 0; i < countMap[num]; i++) {
-                pairs.push([num, reversed]);
-            }
-        } else if (nums.includes(reversed)) {
-            // Add pairs respecting occurrences
-            let count = Math.min(countMap[num] || 0, countMap[reversed] || 0);
-            for (let i = 0; i < count; i++) {
-                pairs.push([num, reversed]);
-                pairs.push([reversed, num]);
-            }
+    nums.forEach(num => {
+        const revNum = parseInt(num.toString().split('').reverse().join(''), 10);
+        if (numSet.has(revNum)) {
+            result.push([num, revNum]);
         }
+    });
 
-        // Mark both numbers as processed
-        seen.add(num);
-        seen.add(reversed);
-    }
-
-    return pairs;
+    return result;
 }
 
-let nums = [12, 21, 33, 121, 345, 131, 456, 787];
+const nums = [12, 21, 33, 121, 345, 131, 456, 787];
 console.log(solution(nums));
+
+// function solution(numbers) {
+//     let pairs = [];
+//     let seen = new Set();
+//     let reverseMap = new Map();
+
+//     for (let num of numbers) {
+//         let reversed = parseInt(num.toString().split("").reverse().join(""));
+//         reverseMap.set(num, reversed);
+//     }
+
+//     for (let num of numbers) {
+//         if (seen.has(num)) continue;
+
+//         let reversed = reverseMap.get(num);
+
+//         if (num === reversed) {
+//             let count = numbers.filter((n) => n === num).length;
+//             for (let i = 0; i < count; i++) {
+//                 pairs.push([num, num]);
+//             }
+//         } else if (numbers.includes(reversed)) {
+//             let countOriginal = numbers.filter((n) => n === num).length;
+//             let countReversed = numbers.filter((n) => n === reversed).length;
+//             let pairCount = Math.min(countOriginal, countReversed);
+
+//             for (let i = 0; i < pairCount; i++) {
+//                 pairs.push([num, reversed]);
+//                 pairs.push([reversed, num]);
+//             }
+//         }
+
+//         seen.add(num);
+//         seen.add(reversed);
+//     }
+
+//     return pairs;
+// }
+
 
