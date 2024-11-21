658e8bc4e4c245cf2886f15965a04cfe41ab7204
kenyansa <kenyansajustine@gmail.com>
2024-11-21 13:46:36 +0300

    feat: :sparkles: add oppositePairSums algo

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -35,6 +35,7 @@ function solution(nums) {
 const nums = [12, 21, 33, 121, 345, 131, 456, 787];
 console.log(solution(nums));
 
+// solution 2:
 // function solution(numbers) {
 //     let pairs = [];
 //     let seen = new Set();
@@ -74,3 +75,18 @@ console.log(solution(nums));
 // }
 
 
+// You are given an array of n integers, where n ranges from 2 to 200, inclusive. The elements in the array range from -200 to 200, inclusive. Your task is to return an array where each element is the sum of a pair composed of an element and its 'opposite' element. By 'opposite', we mean that in an array of n elements, the first and last elements are paired, the second and second-to-last elements are paired, and so on. If the array is of odd length, the middle element pairs with itself. The method should handle both positive and negative integers and be capable of dealing with an odd number of elements in the list. For example, given an input array [1, 2, 3, 4, 5], your method should return [6, 6, 6]. This is because the first element 1 plus the last element 5 equals 6, the second element 2 plus the second-to-last element 4 equals 6, and the middle element 3 plus itself equals 6.
+
+function oppositePairSums(arr) {
+    const result = [];
+    const n = arr.length;
+
+    for (let i = 0; i < Math.ceil(n / 2); i++) {
+        result.push(arr[i] + arr[n - 1 - i]);
+    }
+
+    return result;
+}
+
+let arr = [-200, -100, -50, 2, 5, 150, 200]
+console.log(oppositePairSums(arr))
